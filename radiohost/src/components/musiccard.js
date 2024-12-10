b75d0f60c345bbcb3254168894e9ae428a604a75
kylechu8092 <kc5548092@gmail.com>
2024-12-10 05:35:04 -0800

    Fixed JSON output, finished result page, Map new card component

---
+++ radiohost/src/components/musiccard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { MdOutlinePlaylistAdd, MdOutlineAddCircleOutline } from "react-icons/md";
+import "../styles/songcard.css";
+
+const MusicCard = ({ item, type = "song", showAddButton = true, onClick }) => {
+  const isSong = type === "song";
+
+  return (
+    <div className="song-card" onClick={onClick}>
+     
+
+      {/* Info */}
+      <div className="song-info">
+        <h4 className="song-title">{isSong ? item.songName : item.albumName}</h4>
+        <p className="song-artist">
+          {isSong
+            ? item.artist.map((artist, index) => (
+                <span key={artist.id}>
+                  {artist.name}
+                  {index < item.artist.length - 1 && ", "}
+                </span>
+              ))
+            : item.artist}
+        </p>
+      </div>
+
+      {/* Actions */}
+      {showAddButton && isSong && (
+        <div className="song-actions">
+          <button className="song-button">
+            <MdOutlineAddCircleOutline />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MusicCard;
